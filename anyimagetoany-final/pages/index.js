7d7629515fc8de02a1a6c54e295c5098a636214b
yogeshdhull804 <dhullyogesh122@gmail.com>
2025-07-15 13:57:46 +0530

    Add files via upload

---
+++ anyimagetoany-final/pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Welcome to AnyImageToAny</h1>
+      <p>Upload an image or PDF and convert it easily!</p>
+    </div>
+  );
+}
